148f940ff47658d4bb8112dfc10fb7d43843b93f
BrendanKelly84213 <brewkelly@gmail.com>
2021-02-22 09:14:37 -0800

    removed redundent room description

--- rooms-tariff.php
+++ rooms-tariff.php
@@ -17,10 +17,10 @@
 <!-- form -->
 
 <div class="row">
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Single</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="single-details.php" class="btn btn-default">Check Details</a></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Double</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="double-details.php" class="btn btn-default">Check Details</a></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Queen</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="queen-details.php" class="btn btn-default">Check Details</a></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>King</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="king-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Single</h3><a href="single-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Double</h3><a href="double-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Queen</h3><a href="queen-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>King</h3><a href="king-details.php" class="btn btn-default">Check Details</a></div></div></div>
 </div>
 
 </div>
